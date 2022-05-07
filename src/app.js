871a996269e4253c73959c1dfc8515ad24636735
EstherJL <esther_2629@hotmail.com>
2022-05-07 16:49:52 +0000

    Final

--- src/app.js
+++ src/app.js
@@ -5,7 +5,36 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = () => {
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#excusando").innerHTML = excusa();
+  });
   //write your code here
   console.log("Hello Rigo from the console!");
+};
+
+let excusa = () => {
+  let sujeto = ["Mi hermano", "El gato", "La vecina", "Mi esposo"];
+  let accion = ["rompio", "perdio", "olvido", "se comio"];
+  let que = ["mi tarea"];
+  let cuando = [
+    "justo antes de salir",
+    "ayer por la noche",
+    "hace un instante"
+  ];
+
+  let sujetoIndex = Math.floor(Math.random() * sujeto.length);
+  let accionIndex = Math.floor(Math.random() * accion.length);
+  let queIndex = Math.floor(Math.random() * que.length);
+  let cuandoIndex = Math.floor(Math.random() * cuando.length);
+
+  return (
+    sujeto[sujetoIndex] +
+    " " +
+    accion[accionIndex] +
+    " " +
+    que[queIndex] +
+    " " +
+    cuando[cuandoIndex]
+  );
 };
